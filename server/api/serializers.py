e267b3984fbee92a940abba426639daf02be65dc
brunosousa09 <bruno-boy-sousa@hotmail.com>
2025-12-09 21:34:23 -0300

    Correcao de deploy

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -41,7 +41,7 @@ class TransacaoSerializer(serializers.ModelSerializer):
         return data
 
 class NotificacaoSerializer(serializers.ModelSerializer):
-    
+
     criado_em = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
